26b84bc332b83356b558b8e87d8aa4cdc086c497
Nicole Souza <nicsousan@gmail.com>
2025-05-04 00:10:19 -0300

    31/05 - desktop vesion

---
+++ sua-casa-conectada-web-site/src/components/Carrossel.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import slider from '../../public/Slider.svg'
+import Image from 'next/image'
+
+
+export const Carrossel = () => {
+    return (
+        <div className="relative w-full h-[500px]">
+
+            <div className='relative z-10 flex flex-col items-center justify-center h-full text-blue-400 '>
+                <h1 className='text-5xl font-sans text-center'>Preparamos sua casa para o futuro,</h1>
+                <h1 className='text-5xl p-8 font-sans'>e o futuro é agora!</h1>
+                <div className=' flex gap-4'>
+                    <button className="btn btn-info">Conheça nossas soluções</button>
+                    <button className="btn btn-outline btn-info">Conheça nossos serviços</button>
+                </div>
+            </div>
+            <Image
+                src={slider}
+                alt="Fundo"
+                className="absolute inset-0 w-full h-full object-cover justify-center"
+            />
+        </div>
+    )
+}
---
+++ sua-casa-conectada-web-site/src/components/Subtitle.tsx
@@ -0,0 +1,26 @@
+import Image from 'next/image'
+import React from 'react'
+
+type SubtitleProps = {
+    title: string,
+    descriple: string
+    backgroungImage: string
+}
+
+export const Subtitle = ({title, descriple, backgroungImage}:SubtitleProps) => {
+    return (
+        <div className='relative w-full h-auto p-12'>
+            <div className='relative z-10 flex flex-col items-center justify-center h-full text-blue-400 gap-4'>
+                <h2 className='text-5xl font-sans'>{title}</h2>
+                <p className='text-white font-mono  text-center'>{descriple}</p>
+            </div>
+            <img
+                
+                src={backgroungImage}
+                alt="Fundo"
+                className="absolute inset-0 w-full h-full object-cover"
+                
+            />
+        </div>
+    )
+}
