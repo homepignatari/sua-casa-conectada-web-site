a1c2abde830506293c9d4202d6dd9795e39ee097
Nicole Souza <nicsousan@gmail.com>
2025-05-05 18:13:41 -0300

    05/05 - ajustes de contato

--- sua-casa-conectada-web-site/src/app/Contato/Page.tsx
+++ sua-casa-conectada-web-site/src/app/Contato/Page.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import { Header } from '@/components/Header'
-import { ContactBar } from '@/components/ContactBar'
 import FooterBar from '@/components/FooterBar'
 
 export default function page() {
